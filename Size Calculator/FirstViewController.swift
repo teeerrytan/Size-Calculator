c4630cb7b352a7d4aad269c5a335047c1e0f9907
Terry Tan <terrytan@terrytan.cc>
2018-07-08 02:18:45 +0800

    Initial Commit

---
+++ Size Calculator/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Size Calculator
+//
+//  Created by 谈铭瑶 on 2018/7/8.
+//  Copyright © 2018年 Terry Tan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
